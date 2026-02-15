8b8a89a60e8726f789e2168e7a048002b1842052
Hamzah-Amir <hamzaamir7651@gmail.com>
2026-02-15 15:19:49 +0500

    called summary method in search method

--- search_api.py
+++ search_api.py
@@ -14,7 +14,9 @@ class NexaSearch:
             model="gemini-2.5",
             contents=f"You are an expert researcher and give information on facts only, answr this query: \"{query}\" and you last limit of maximum sentences is 12 you always try to give shortest and to the point answer to the query and barely use 9 to 12 sentences."
         )
-        return {"response": response.text}
+        data = response.text
+        summary = self.summarize(data) 
+        return {"response": summary}
     
     def generate_image(self, query):
         response = self.client.models.generate_image(
@@ -29,3 +31,5 @@ class NexaSearch:
             model="gemini-2.5",
             content=f"You are an Nexa AI powered desktop assistant, make a well defined and top notch summary of this query: {query} not more than 7 sentences and make summary without commentary"
         )
+
+        return summary.text
